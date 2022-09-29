b0906e0552772285ec26234551fcba1f7dae8822
Nusurmatov Husniddin <NusurmatovPro@gmail.com>
2022-09-29 11:31:41 +0500

    solve task in OOP module

---
+++ OOP/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading;
+
+namespace CarPark
+{
+    public class Program
+    {
+        private static Random generator = new Random(); 
+        
+        public static void Main(string[] args)
+        {
+            SetShriftColor();
+            var passengerCar = new Vehicle.PassengerCar();
+            Console.Write(passengerCar.GetInfo());
+            MakeHimOrHerWait();
+            
+            SetShriftColor();
+            var truck = new Vehicle.Truck();
+            Console.Write(truck.GetInfo());
+            MakeHimOrHerWait();
+            
+            SetShriftColor();
+            var bus = new Vehicle.Bus();
+            Console.Write(bus.GetInfo());
+            MakeHimOrHerWait();
+            
+            SetShriftColor();
+            var scooter = new Vehicle.Scooter();
+            Console.Write(scooter.GetInfo());
+            MakeHimOrHerWait();
+
+            SetShriftColor();
+            Console.Write("The End...!");
+        }
+
+        private static void SetShriftColor()
+        {
+            Console.Clear();
+            Console.ForegroundColor = (ConsoleColor) generator.Next(1, 16);  // random shrift color
+        }
+
+        private static void MakeHimOrHerWait()
+        {
+            while (true)
+            {
+                string input = String.Empty;
+
+                Console.Write("\n\n\nYou have 10 secunds to read the above content: ");
+
+                for (int i = 1; i < 11; i++)
+                {
+                    Console.Write($"{i} ");
+                    Thread.Sleep(1000);
+                }
+
+                break;
+            }
+        }
+    }
+}
