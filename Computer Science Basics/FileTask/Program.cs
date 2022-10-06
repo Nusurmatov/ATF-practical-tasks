719a7944f406bf560a86de0bafe08f30ce870e27
Nusurmatov Husniddin <NusurmatovPro@gmail.com>
2022-10-06 16:41:40 +0500

    add 3 small solved problems on Computer Science Basics Module

---
+++ Computer Science Basics/FileTask/Program.cs
@@ -0,0 +1,57 @@
+﻿class PizzaDelivery
+{
+    static void Main()
+    {
+        System.IO.File.WriteAllText("waitingList.txt", "No orders yet.\n");
+
+        Console.Clear();
+        bool undone = true;
+        var message = new System.Text.StringBuilder();
+        int option = 0;
+
+        while (undone)
+        {
+            message.AppendLine("1) Add an order to the queue.");
+            message.AppendLine("2) Delete the order from the queue.");
+            message.AppendLine("3) Number of orders.");
+            message.AppendLine("4) Display order list.");
+            message.AppendLine("0) Exit the program. (This is the default option)");
+            message.Append("   Choose one of this options: ");
+            Console.Write(message);
+
+            if (int.TryParse(Console.ReadLine(), out option))
+            {
+                switch (option)
+                {
+                    case 1:
+                        Order.AddToWaitingList();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Order.DeleteFromWaitingList();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Number of clients waiting: {0}.\n", Order.OrderList.Count);
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Order.DisplayWaitingList();
+                        break;
+                    default:
+                        undone = false;
+                        Console.WriteLine("\nThank you...!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid Input. Try Again!\n");
+            }
+            
+            message.Clear();
+        }
+    }
+}
---
+++ Computer Science Basics/SortingTask/Program.cs
@@ -0,0 +1,55 @@
+﻿class DonutCompetition
+{
+    static Random Random = new Random();
+    static int[] results;
+    
+    static void Main()
+    {
+        Console.Clear();
+        Console.Write("This is the 'Donut Competitoin'. To win this competition ");
+        Console.WriteLine("participants should as many donuts as possible in 10 minutes.");
+        bool undone = true;
+        int N;
+
+        while (undone)
+        {
+            Console.Write("How many participants do you like to participate in this: ");
+            
+            if (int.TryParse(Console.ReadLine(), out N))
+            {
+                results = new int[N];
+                StartCompetition(N);
+                undone = false;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid Input. Try Again!");
+            }
+        }
+    }
+
+    static void StartCompetition(int n)
+    {
+        Console.ForegroundColor = (ConsoleColor) Random.Next(1, 14);
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Clear();
+            Console.WriteLine($"Time: {i}");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"Number of eaten donuts by participant {j+1}: ");
+                Console.WriteLine((results[j] += Random.Next(1, 4)));
+            }
+            Thread.Sleep(1500);
+        } 
+
+        Array.Sort(results);
+        Console.WriteLine($"\nThe winner has eaten {results[^1]} donuts.");
+        Console.WriteLine($"The runner-up has eaten {results[^2]} donuts.");
+        Console.WriteLine($"The third result is {results[^3]} donuts.");
+        Console.WriteLine($"And the worst result is {results[0]} donuts.\n");
+    }
+}
